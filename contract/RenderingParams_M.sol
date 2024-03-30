7e7c6669fdfcaff4176b6f24bbd07b8388e40121
Sepang2 <sepang296@gmail.com>
2024-03-31 07:05:10 +0900

    Add Merchandise contracts

--- contract/RenderingParams_M.sol
+++ contract/RenderingParams_M.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-struct RenderParams {
+struct RenderParams_M {
     string musician;
     string title;
-    string initial;
-    string email;
     string location;
     string date;
 }
